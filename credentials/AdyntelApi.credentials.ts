c83d99d895ddfab8391c29bde54e8cb9235116cd
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2026-01-09 00:23:08 +0100

    Add credential test property to AdyntelApi credentials

--- credentials/AdyntelApi.credentials.ts
+++ credentials/AdyntelApi.credentials.ts
@@ -5,8 +5,9 @@ import {
 } from 'n8n-workflow';
 
 export class AdyntelApi implements ICredentialType {
-	name = 'AdyntelApi';
+	name = 'adyntelApi';
 	displayName = 'Adyntel API';
+	// eslint-disable-next-line n8n-nodes-base/cred-class-field-documentation-url-miscased
 	documentationUrl = 'https://docs.adyntel.com';
 	properties: INodeProperties[] = [
 		{
@@ -20,6 +21,7 @@ export class AdyntelApi implements ICredentialType {
 			displayName: 'Email',
 			name: 'email',
 			type: 'string',
+			placeholder: 'name@email.com',
 			default: '',
 		},
 	];
@@ -30,6 +32,17 @@ export class AdyntelApi implements ICredentialType {
 				'api_key': '={{$credentials.apiKey}}',
 				'email': '={{$credentials.email}}'
 			}
+		},
+	};
+	test = {
+		request: {
+			baseURL: 'https://api.adyntel.com',
+			url: '/test',
+			method: 'POST' as const,
+			body: {
+				'api_key': '={{$credentials.apiKey}}',
+				'email': '={{$credentials.email}}',
+			},
 		},
 	};
 }
