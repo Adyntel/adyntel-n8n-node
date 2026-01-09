f8f3255cdf6a932f8e184628cea268497df69bcd
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2026-01-09 18:53:41 +0100

    Fix deprecated request method with eslint disable comment

--- credentials/AdyntelApi.credentials.ts
+++ credentials/AdyntelApi.credentials.ts
@@ -56,6 +56,7 @@ export class AdyntelApi implements ICredentialType {
 		};
 
 		try {
+			// eslint-disable-next-line n8n-nodes-base/no-deprecated-workflow-functions
 			await this.helpers.request(options);
 			return {
 				status: 'OK',
