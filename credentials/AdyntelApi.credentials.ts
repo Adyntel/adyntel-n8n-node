0a65918b160aed2d9d48eecc0f5d628c4d32e91a
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2026-01-16 23:42:10 +0100

    Add icon property to credentials, usableAsTool to node, and fix linting issues

--- credentials/AdyntelApi.credentials.ts
+++ credentials/AdyntelApi.credentials.ts
@@ -3,53 +3,52 @@ import {
 	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
+	Icon,
 } from 'n8n-workflow';
 
 export class AdyntelApi implements ICredentialType {
 	name = 'adyntelApi';
 	displayName = 'Adyntel API';
-	// eslint-disable-next-line n8n-nodes-base/cred-class-field-documentation-url-miscased
 	documentationUrl = 'https://docs.adyntel.com';
-  
+	icon: Icon = 'file:adyntel.svg';
+
 	properties: INodeProperties[] = [
-	  {
-		displayName: 'API Key',
-		name: 'apiKey',
-		type: 'string',
-		typeOptions: { password: true },
-		default: '',
-	  },
-	  {
-		displayName: 'Email',
-		name: 'email',
-		type: 'string',
-		placeholder: 'name@email.com',
-		default: '',
-	  },
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+		{
+			displayName: 'Email',
+			name: 'email',
+			type: 'string',
+			placeholder: 'name@email.com',
+			default: '',
+		},
 	];
-  
+
 	authenticate: IAuthenticateGeneric = {
-	  type: 'generic',
-	  properties: {
-		body: {
-		  api_key: '={{$credentials.apiKey}}',
-		  email: '={{$credentials.email}}',
+		type: 'generic',
+		properties: {
+			body: {
+				api_key: '={{$credentials.apiKey}}',
+				email: '={{$credentials.email}}',
+			},
 		},
-	  },
 	};
-  
+
 	test: ICredentialTestRequest = {
-	  request: {
-		method: 'POST',
-		url: 'https://api.adyntel.com/facebook',
-		headers: {
-		  'Content-Type': 'application/json',
+		request: {
+			baseURL: 'https://api.adyntel.com',
+			url: '/facebook',
+			method: 'POST',
+			body: {
+				api_key: '={{$credentials.apiKey}}',
+				email: '={{$credentials.email}}',
+				company_domain: 'lokalise.com',
+			},
 		},
-		body: {
-		  api_key: '={{$credentials.apiKey}}',
-		  email: '={{$credentials.email}}',
-		  company_domain: 'lokalise.com',
-		},
-	  },
 	};
-  }
+}
